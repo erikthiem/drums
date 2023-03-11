514675875fca0cfe1e1466de02ebca9430607562
Erik Thiem <erikthiem@gmail.com>
2023-03-11 18:17:45 -0500

    Try to get it to support ios 13+

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-if ('Accelerometer' in window) {
-	let sensor = new Accelerometer({frequency: 60});
-	sensor.addEventListener('reading', function() {
-		var x = sensor.x.toFixed(2);
-		var y = sensor.y.toFixed(2);
-		var z = sensor.z.toFixed(2);
-		document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
-	});
-	sensor.start();
+if (typeof DeviceMotionEvent.requestPermission === 'function') {
+  // iOS 13+
+  DeviceMotionEvent.requestPermission().then(permissionState => {
+    if (permissionState === 'granted') {
+      let sensor = new Accelerometer({frequency: 60});
+      sensor.addEventListener('reading', function() {
+        var x = sensor.x.toFixed(2);
+        var y = sensor.y.toFixed(2);
+        var z = sensor.z.toFixed(2);
+        document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
+      });
+      sensor.start();
+    } else {
+      document.getElementById('data').innerHTML = 'Accelerometer permission not granted.';
+    }
+  }).catch(console.error);
 } else {
-	document.getElementById('data').innerHTML = 'Accelerometer not supported.';
+  // Non-iOS 13+ devices
+  if ('DeviceMotionEvent' in window) {
+    let sensor = new Accelerometer({frequency: 60});
+    sensor.addEventListener('reading', function() {
+      var x = sensor.x.toFixed(2);
+      var y = sensor.y.toFixed(2);
+      var z = sensor.z.toFixed(2);
+      document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
+    });
+    sensor.start();
+  } else {
+    document.getElementById('data').innerHTML = 'Accelerometer not supported.';
+  }
 }
