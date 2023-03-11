73fc2390f17869143b2a5cfa62b472982e149238
Erik Thiem <erikthiem@gmail.com>
2023-03-11 18:12:22 -0500

    Try using a different sensor way

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-window.addEventListener("devicemotion", function(event) {
-  var acceleration = event.accelerationIncludingGravity;
-  var x = acceleration.x.toFixed(2);
-  var y = acceleration.y.toFixed(2);
-  var z = acceleration.z.toFixed(2);
-  document.getElementById("x").innerHTML = x;
-  document.getElementById("y").innerHTML = y;
-  document.getElementById("z").innerHTML = z;
-});
+if ('Accelerometer' in window) {
+	let sensor = new Accelerometer({frequency: 60});
+	sensor.addEventListener('reading', function() {
+		var x = sensor.x.toFixed(2);
+		var y = sensor.y.toFixed(2);
+		var z = sensor.z.toFixed(2);
+		document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
+	});
+	sensor.start();
+} else {
+	document.getElementById('data').innerHTML = 'Accelerometer not supported.';
+}
