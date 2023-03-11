7396ca8f367ddc13ea617ca7793c725a15f0e70d
Erik Thiem <erikthiem@gmail.com>
2023-03-11 18:22:00 -0500

    Try again

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-if (typeof DeviceMotionEvent.requestPermission === 'function') {
+let x = document.getElementById("x");
+let y = document.getElementById("y");
+let z = document.getElementById("z");
+
+if (typeof(DeviceMotionEvent) !== "undefined" && typeof(DeviceMotionEvent.requestPermission) === "function") {
   // iOS 13+
-  DeviceMotionEvent.requestPermission().then(permissionState => {
-    if (permissionState === 'granted') {
-      let sensor = new Accelerometer({frequency: 60});
-      sensor.addEventListener('reading', function() {
-        var x = sensor.x.toFixed(2);
-        var y = sensor.y.toFixed(2);
-        var z = sensor.z.toFixed(2);
-        document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
+  DeviceMotionEvent.requestPermission().then(response => {
+    if (response === 'granted') {
+      window.addEventListener('devicemotion', e => {
+        x.innerHTML = e.accelerationIncludingGravity.x.toFixed(2);
+        y.innerHTML = e.accelerationIncludingGravity.y.toFixed(2);
+        z.innerHTML = e.accelerationIncludingGravity.z.toFixed(2);
       });
-      sensor.start();
-    } else {
-      document.getElementById('data').innerHTML = 'Accelerometer permission not granted.';
     }
   }).catch(console.error);
 } else {
-  // Non-iOS 13+ devices
-  if ('DeviceMotionEvent' in window) {
-    let sensor = new Accelerometer({frequency: 60});
-    sensor.addEventListener('reading', function() {
-      var x = sensor.x.toFixed(2);
-      var y = sensor.y.toFixed(2);
-      var z = sensor.z.toFixed(2);
-      document.getElementById('data').innerHTML = 'X: ' + x + '<br>Y: ' + y + '<br>Z: ' + z;
-    });
-    sensor.start();
-  } else {
-    document.getElementById('data').innerHTML = 'Accelerometer not supported.';
-  }
+  // non-iOS 13+ and non-iOS devices
+  window.addEventListener('devicemotion', e => {
+    x.innerHTML = e.accelerationIncludingGravity.x.toFixed(2);
+    y.innerHTML = e.accelerationIncludingGravity.y.toFixed(2);
+    z.innerHTML = e.accelerationIncludingGravity.z.toFixed(2);
+  });
 }
